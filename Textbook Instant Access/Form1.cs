bdf813345eb51d6dd3279d960583f466634b8021
Edward Webb <Edward Webb>
2016-10-03 16:17:06 +1100

    Added storing paths in database with SQLite

--- Textbook Instant Access/Form1.cs
+++ Textbook Instant Access/Form1.cs
@@ -9,21 +9,26 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
+using System.Data.SQLite;
 
 namespace Textbook_Instant_Access
 {
     public partial class Form1 : Form
     {
+        SQLiteConnection m_dbConnection;
         public Form1()
         {
             InitializeComponent();
-            
+            m_dbConnection = new SQLiteConnection("Data Source=paths.db;Version=3;");
+            m_dbConnection.Open();
         }
 
         private void addButton_Click(object sender, EventArgs e)
         {
             quickAccessBox.Items.Add(addTextBox.Text);
-            
+            string sql = "insert into PATHS (PATH) values ('" + addTextBox.Text + "')";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
         }
 
         private void quickAccessBox_doubleClick(object sender, EventArgs e)
@@ -52,13 +57,20 @@ namespace Textbook_Instant_Access
         private void formLoaded(object sender, EventArgs e)
         {
             // Read saved entries from database
+            string sql = "select * from PATHS";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                quickAccessBox.Items.Add(reader["PATH"]);
         }
 
         private void removeButton_Click(object sender, EventArgs e)
         {
             string selected = quickAccessBox.SelectedItem.ToString();
             quickAccessBox.Items.Remove(quickAccessBox.SelectedItem);
-           
+            string sql = "delete from PATHS where PATH = '" + selected + "'";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
         }
     }
 }
